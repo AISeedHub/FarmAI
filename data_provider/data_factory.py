5b96756304250ab352b9e085d8ee667cdc86ab58
Andrew <andrewlee1807@gmail.com>
2025-05-28 15:50:53 +0900

    Refactor data factory and implement MultiEpochsDataLoader
    Introduced the MultiEpochsDataLoader class to enable infinite iteration across epochs, providing better flexibility and customization. Simplified the `data_provider` function by unifying logic and refining dataset creation and loader configuration. Removed redundant and commented-out code for improved clarity and maintainability.

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,4 +1,4 @@
-from data_provider.data_loader import (Dataset_Custom, Dataset_Pred,
+from data_provider.data_loader import (Dataset_Custom,
                                        Dataset_TextPrompt)
 from torch.utils.data import DataLoader
 
@@ -24,6 +24,22 @@ class _RepeatSampler(object):
 
 
 class MultiEpochsDataLoader(DataLoader):
+    """A custom DataLoader that ensures infinite iteration for multiple epochs.
+    
+    This class wraps PyTorch's DataLoader to provide infinite iteration capability by 
+    reusing the batch sampler across epochs. It maintains internal state to track 
+    initialization and iteration.
+
+    Args:
+        *args: Variable length argument list to be passed to DataLoader.
+        **kwargs: Arbitrary keyword arguments to be passed to DataLoader.
+        
+    Example:
+        >>> dataset = Dataset_Custom()
+        >>> loader = MultiEpochsDataLoader(dataset, batch_size=32, shuffle=True)
+        >>> for batch in loader:
+        >>>     # Process batch
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,9 +49,19 @@ class MultiEpochsDataLoader(DataLoader):
         self.iterator = super().__iter__()
 
     def __len__(self):
+        """Returns the number of batches in a single epoch.
+        
+        Returns:
+            int: Length of the batch sampler for one epoch.
+        """
         return len(self.batch_sampler.sampler)
 
     def __iter__(self):
+        """Creates an iterator that yields batches of data.
+        
+        Yields:
+            Any: Next batch of data from the dataset.
+        """
         for i in range(len(self)):
             yield next(self.iterator)
 
@@ -43,41 +69,21 @@ class MultiEpochsDataLoader(DataLoader):
 def data_provider(args, flag):
     Dataset = data_dict[args.data]
     timeenc = 0 if args.embed != 'timeF' else 1
+    shuffle_flag = False if (flag == 'test' or flag == 'TEST') else True
+    drop_last = False
+    batch_size = args.batch_size
+    freq = args.freq
 
-    if flag == 'test':
-        shuffle_flag = False
-        drop_last = True
-        batch_size = args.batch_size
-        freq = args.freq
-    elif flag == 'pred':
-        shuffle_flag = False
-        drop_last = False
-        batch_size = 1
-        freq = args.detail_freq
-        Dataset = Dataset_Pred
-    else:
-        shuffle_flag = True
-        drop_last = True
-        batch_size = args.batch_size
-        freq = args.freq
-
-    if args.data == 'GY':
-        data_set = Dataset(args.root_path,
-                        flag=flag,
-                        inp_seq_len=96,
-                        label_seq_en=48,
-                        pred_seq_len=24)
-    else:
-        data_set = Dataset(
-            root_path=args.root_path,
-            data_path=args.data_path,
-            flag=flag,
-            size=[args.seq_len, args.label_len, args.pred_len],
-            features=args.features,
-            target=args.target,
-            timeenc=timeenc,
-            freq=freq
-        )
+    data_set = Dataset(
+        root_path=args.root_path,
+        data_path=args.data_path,
+        flag=flag,
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        timeenc=timeenc,
+        freq=freq
+    )
     print(flag, len(data_set))
 
     data_loader = MultiEpochsDataLoader(data_set,
@@ -85,13 +91,4 @@ def data_provider(args, flag):
                                         shuffle=shuffle_flag,
                                         drop_last=drop_last)
 
-    # data_loader = DataLoader(
-    #     data_set,
-    #     batch_size=batch_size,
-    #     shuffle=shuffle_flag,
-    #     num_workers=16,
-    #     pin_memory=True,
-    #     persistent_workers=True,
-    #     multiprocessing_context='spawn',
-    #     drop_last=drop_last)
     return data_set, data_loader
